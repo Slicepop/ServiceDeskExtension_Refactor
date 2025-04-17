becdb02e8257e3b826a7b806dcb31491cf82d847
Slicepop <kaden.seeger@gmail.com>
2025-04-17 17:02:28 -0400

    implement fetchTitle function to update document title based on request id

--- Extension/openedRequest.js
+++ Extension/openedRequest.js
@@ -1,3 +1,21 @@
 function runRequestTab() {
-  console.log("LookupRequest?");
+  fetchTitle();
+}
+async function fetchTitle() {
+  const itemID = window.location.href.split("requestId=")[1];
+  try {
+    const response = await fetch(
+      "https://support.wmed.edu/LiveTime/services/v1/user/requests/" +
+        itemID +
+        "/basic",
+      {
+        headers: {
+          accept: "application/json, text/plain, */*",
+          "zsd-source": "LT",
+        },
+      }
+    );
+    const data = await response.json();
+    document.title = data.subject;
+  } catch (error) {}
 }
