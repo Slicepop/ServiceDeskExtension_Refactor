1aeed86934b8cf5d13649b03aa130b7456fd4baa
Slicepop <kaden.seeger@gmail.com>
2025-04-18 13:27:00 -0400

    add Save/Close button functionality + add styles.css

--- Extension/main.js
+++ Extension/main.js
@@ -1,39 +1,42 @@
 if (window.location.href.includes("LookupRequest?")) {
+  // if you have a request opened
   runRequestTab();
-}
-const incident = document.querySelector(
-  ".rowoverride > div.mb-3.col-10 > ul > li:nth-child(2)"
-);
-if (incident) {
-  const incidentNum = incident.querySelector("sup");
-  setTimeout(() => {
-    if (incidentNum && incidentNum.textContent !== "0") {
-      incident.click();
-    }
-  }, 1);
-}
+} else {
+  // support site not looking at individual request
+  const incident = document.querySelector(
+    ".rowoverride > div.mb-3.col-10 > ul > li:nth-child(2)"
+  );
+  if (incident) {
+    const incidentNum = incident.querySelector("sup");
+    setTimeout(() => {
+      if (incidentNum && incidentNum.textContent !== "0") {
+        incident.click();
+      }
+    }, 1);
+  }
 
-function replaceLinks() {
-  const linksToRequest = document.querySelectorAll("#requestId");
-  linksToRequest.forEach((link) => {
-    const newLink = document.createElement("a");
-    newLink.textContent = link.textContent.trim();
-    newLink.href = `https://support.wmed.edu/LiveTime/WebObjects/LiveTime.woa/wa/LookupRequest?sourceId=New&requestId=${newLink.textContent}`;
-    newLink.target = "_blank";
-    newLink.dataset.processed = true;
-    link.parentNode.replaceChild(newLink, link);
+  function replaceLinks() {
+    const linksToRequest = document.querySelectorAll("#requestId");
+    linksToRequest.forEach((link) => {
+      const newLink = document.createElement("a");
+      newLink.textContent = link.textContent.trim();
+      newLink.href = `https://support.wmed.edu/LiveTime/WebObjects/LiveTime.woa/wa/LookupRequest?sourceId=New&requestId=${newLink.textContent}`;
+      newLink.target = "_blank";
+      newLink.dataset.processed = true;
+      link.parentNode.replaceChild(newLink, link);
+    });
+  }
+
+  let debounceTimeout;
+  const observer = new MutationObserver(() => {
+    clearTimeout(debounceTimeout);
+    debounceTimeout = setTimeout(() => {
+      replaceLinks();
+    }, 10);
+  });
+
+  observer.observe(document.body, {
+    childList: true,
+    subtree: true,
   });
 }
-
-let debounceTimeout;
-const observer = new MutationObserver(() => {
-  clearTimeout(debounceTimeout);
-  debounceTimeout = setTimeout(() => {
-    replaceLinks();
-  }, 1);
-});
-
-observer.observe(document.body, {
-  childList: true,
-  subtree: true,
-});
--- Extension/openedRequest.js
+++ Extension/openedRequest.js
@@ -1,5 +1,32 @@
 function runRequestTab() {
   fetchTitle();
+
+  let debounceTimeout;
+  const observer = new MutationObserver(() => {
+    clearTimeout(debounceTimeout);
+    debounceTimeout = setTimeout(() => {
+      addSaveCloseBTN();
+    }, 10);
+  });
+
+  observer.observe(document.body, {
+    childList: true,
+    subtree: true,
+  });
+}
+function addSaveCloseBTN() {
+  if (document.querySelector("#Save_Close")) {
+    return;
+  }
+  const test = document.createElement("p");
+  test.id = "Save_Close";
+  test.textContent = "SAVE\n+\nCLOSE";
+  test.title = "Saves Ticket and Closes Window";
+  document
+    .querySelector(
+      "#request_general_container > div > div.card-header.general-card-header > button"
+    )
+    .appendChild(test);
 }
 async function fetchTitle() {
   const itemID = window.location.href.split("requestId=")[1];
