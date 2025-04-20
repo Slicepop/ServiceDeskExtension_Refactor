69a07299c2a591ab4ad50fa509d4ec7640740afe
kseege3 <kseege3@wgu.edu>
2025-04-19 22:57:56 -0400

    add functionality to assign macro report value and navigate to default report

--- Extension/functions.js
+++ Extension/functions.js
@@ -1,3 +1,84 @@
+function assignMacroReportValue() {
+  chrome.storage.local.get("macroReport", (response) => {
+    const MacroOn = response.macroReport;
+    if (!MacroOn)
+      chrome.storage.local.set(
+        { macroReport: "false" },
+        assignMacroReportValue
+      );
+
+    const secondSubMenuItem = document.querySelector(
+      "#submenu > li:nth-child(1) > a"
+    );
+    const fourthSubMenuItem = document.querySelector(
+      "#submenu > li:nth-child(5) > a"
+    );
+    const KPIreportSelected =
+      secondSubMenuItem &&
+      secondSubMenuItem.className === "active" &&
+      secondSubMenuItem.textContent === "KPI";
+    const TechnicianSelected =
+      fourthSubMenuItem &&
+      fourthSubMenuItem.className === "active" &&
+      fourthSubMenuItem.textContent === "Technician";
+    // check if user is in reports page and wants to go to the Requests Status by Technician (Closed) report
+    if (KPIreportSelected && MacroOn) navigateToDefaultReport();
+    else if (TechnicianSelected && MacroOn) selectDefaultReport();
+  });
+}
+function navigateToDefaultReport() {
+  const technicianReports = document.querySelector(
+    "#submenu > li:nth-child(5) > a"
+  );
+  window.location.replace(technicianReports.href);
+}
+function selectDefaultReport() {
+  const changeEvent = new Event("change");
+  const reportSelection = document.querySelector(
+    "#technicianReportsForm > div > div.windowContent > div > table > tbody > tr:nth-child(1) > td.fieldtext > select"
+  );
+  if (!reportSelection) return;
+  else if (reportSelection.selectedIndex == "10") {
+    selectBoundsOfCurrWeek();
+    return;
+  }
+  reportSelection.selectedIndex = "10";
+  reportSelection.dispatchEvent(changeEvent);
+}
+
+function selectBoundsOfCurrWeek() {
+  function getMondayOfCurrentWeek() {
+    const today = new Date();
+    const dayOfWeek = today.getDay(); // Sunday = 0, Monday = 1, etc.
+    const distanceToMonday = dayOfWeek === 0 ? 6 : dayOfWeek - 1; // If Sunday (0), move back 6 days, otherwise move back `dayOfWeek - 1`
+
+    today.setDate(today.getDate() - distanceToMonday);
+    today.setHours(0, 0, 0, 0); // Set time to the start of the day
+    return today;
+  }
+  function getFridayOfCurrentWeek() {
+    const Monday = getMondayOfCurrentWeek();
+    const Friday = new Date(Monday);
+    Friday.setDate(Monday.getDate() + 4);
+    return Friday;
+  }
+  console.log("SD");
+  const Monday = getMondayOfCurrentWeek();
+  const formattedDate = Monday.toLocaleDateString("en-GB").replace(
+    /(\d{2})\/(\d{2})\/(\d{4})/,
+    "$2/$1/$3"
+  );
+  document.querySelector("#startDateUserTZ").value = formattedDate;
+  const Friday = getFridayOfCurrentWeek();
+  const formattedDate2 = Friday.toLocaleDateString("en-GB").replace(
+    /(\d{2})\/(\d{2})\/(\d{4})/,
+    "$2/$1/$3"
+  );
+  document.querySelector("#endDateUserTZ").value = formattedDate2;
+  document.querySelector(
+    "#technicianReportsForm > div > div.windowContent > div > table > tbody > tr:nth-child(4) > td.fieldtext > select"
+  ).selectedIndex = 13;
+}
 function modifyRequestBTN() {
   const navRequestBTN = document.querySelector(
     "#zsd_navbar_menus > ul.navbar-nav.mr-auto > li:nth-child(2) > a"
--- Extension/main.js
+++ Extension/main.js
@@ -1,12 +1,14 @@
+modifyRequestBTN();
 // modifies request button at the top to not have woa/wo/9.0.33.7.1.1 at the end of url
 // woa/wo/9.0.33.7.1.1 causes the session to expire eventually
-modifyRequestBTN();
-// if you have a request opened
+assignMacroReportValue();
+// checks/sets  if user has selected to automatically go to 'Requests Status by Technician (Closed)' report
 if (window.location.href.includes("LookupRequest?")) runRequestTab();
-// if you go to url with '/reports' redirect to the reports page
+// if you have a request opened
 else if (window.location.href.includes("/reports")) clickReportBTN();
-// support site not looking at individual request
+// if you go to url with '/reports' redirect to the reports page
 else {
+  // support site not looking at individual request
   selectDefaultView();
 
   let debounceTimeout;
